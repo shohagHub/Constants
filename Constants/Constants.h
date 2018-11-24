481734abcac3cd40015a20d313c58b772f896215
Saleh Enam Shohag <shohag402@gmail.com>
2018-11-24 12:09:48 +0600

    First Commit
    Signed-off-by: Saleh Enam Shohag <shohag402@gmail.com>

---
+++ Constants/Constants.h
@@ -0,0 +1,14 @@
+//
+//  Constants.h
+//  Constants
+//
+//  Created by Saleh Enam Shohag on 24/11/18.
+//  Copyright © 2018 Nazia Afroz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface Constants : NSObject
+#define IS_IPHONE (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone)
+@end
---
+++ Constants/Constants.m
@@ -0,0 +1,15 @@
+//
+//  Constants.m
+//  Constants
+//
+//  Created by Saleh Enam Shohag on 24/11/18.
+//  Copyright © 2018 Nazia Afroz. All rights reserved.
+//
+
+#import "Constants.h"
+
+@implementation Constants
+-(BOOL)isIphone{
+    return IS_IPHONE;
+}
+@end
